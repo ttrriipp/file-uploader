f6e60483a27c826f2e7187972112da42cac059a5
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-06-24 12:01:29 +0800

    feat: file upload form with multer dependency

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,8 @@ router.post("/register", authController.registerPost);
 router.post("/logout", authController.logoutPost);
 router.get("/login", authController.loginGet);
 router.post("/login", authController.loginPost);
+router.get("/upload-file", (req, res) =>
+  res.render("upload-file", { title: "Upload File" }),
+);
 
 module.exports = router;
