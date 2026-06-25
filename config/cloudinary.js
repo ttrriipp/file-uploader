48f79a3c20575a8a2e083d7f872015e8ace9cd50
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-06-25 11:53:50 +0800

    setup cloudinary

---
+++ config/cloudinary.js
@@ -0,0 +1,13 @@
+require("dotenv/config");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const path = require("path");
+
+// Cloudinary Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+module.exports = cloudinary;
